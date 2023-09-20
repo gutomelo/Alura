032778dbed35ca419c2eaacf6162f2eeb99d4294
Antônio Melo <antonio.melo@clickbus.com>
2023-09-20 09:47:44 -0300

    Update 20092023

--- Java/Java - Aplicando orientacao objetos/screenmatch/src/Principal.java
+++ Java/Java - Aplicando orientacao objetos/screenmatch/src/Principal.java
@@ -1,4 +1,8 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroDeRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -7,6 +11,7 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do Filme: "+ meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
@@ -15,5 +20,33 @@ public class Principal {
         System.out.println("Soma das Avaliações: " + meuFilme.getSomaDasAvaliacoes());
         System.out.println("Total das Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("A média das avaliações " + meuFilme.obterMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("LOST");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodioPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        System.out.println("Duração da Série: "+ lost.getDuracaoEmMinutos());
+
+        Filme outroFilme = new Filme();
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println("Tempo total da Série: " + calculadora.getTempoTotal());
+        FiltroDeRecomendacao filtro = new FiltroDeRecomendacao();
+        filtro.filtra(meuFilme);
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizaoes(300);
+        filtro.filtra(episodio);
     }
 }
