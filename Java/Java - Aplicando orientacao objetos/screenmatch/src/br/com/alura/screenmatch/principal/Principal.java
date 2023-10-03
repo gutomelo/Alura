e1feffb2b9ebabbad657f8b07124d65377b01c02
Antônio Melo <antonio.melo@clickbus.com>
2023-10-02 21:04:36 -0300

    Update Alura 02102023

--- Java/Java - Aplicando orientacao objetos/screenmatch/src/br/com/alura/screenmatch/principal/Principal.java
+++ Java/Java - Aplicando orientacao objetos/screenmatch/src/br/com/alura/screenmatch/principal/Principal.java
@@ -1,15 +1,18 @@
+package br.com.alura.screenmatch.principal;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.calculos.FiltroDeRecomendacao;
 import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
 
-        Filme meuFilme = new Filme();
-        meuFilme.setNome("O poderoso chefão");
-        meuFilme.setAnoDeLancamento(1970);
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        //meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do Filme: "+ meuFilme.getDuracaoEmMinutos());
 
@@ -21,32 +24,44 @@ public class Principal {
         System.out.println("Total das Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("A média das avaliações " + meuFilme.obterMedia());
 
-        Serie lost = new Serie();
-        lost.setNome("LOST");
-        lost.setAnoDeLancamento(2000);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.setDuracaoEmMinutos(200);
+
+
+        Serie lost = new Serie("LOST", 2000);
         lost.exibeFichaTecnica();
         lost.setTemporadas(10);
         lost.setEpisodioPorTemporada(10);
         lost.setMinutosPorEpisodio(50);
-
         System.out.println("Duração da Série: "+ lost.getDuracaoEmMinutos());
 
-        Filme outroFilme = new Filme();
-        outroFilme.setNome("Avatar");
-        outroFilme.setAnoDeLancamento(2023);
-        outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
         calculadora.inclui(meuFilme);
         calculadora.inclui(outroFilme);
         calculadora.inclui(lost);
         System.out.println("Tempo total da Série: " + calculadora.getTempoTotal());
+
         FiltroDeRecomendacao filtro = new FiltroDeRecomendacao();
         filtro.filtra(meuFilme);
+
         Episodio episodio = new Episodio();
         episodio.setNumero(1);
         episodio.setSerie(lost);
         episodio.setTotalVisualizaoes(300);
         filtro.filtra(episodio);
+
+        Filme filmeDoGuto = new Filme("Forest Gump", 2003);
+        filmeDoGuto.setDuracaoEmMinutos(200);
+        filmeDoGuto.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoGuto);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro Filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes.toString());
+
     }
 }
---
+++ Java/Java - Aplicando orientacao objetos/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,33 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        Filme filmeDoGuto = new Filme("Forest Gump", 2003);
+        filmeDoGuto.avalia(10);
+        Serie lost = new Serie("LOST", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoGuto);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista){
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme){
+                System.out.println("Classificacao: " + filme.getClassificacao());
+            }
+
+        }
+
+    }
+}
