203766a697035593319b6ddd8447aac32d9a038b
Antônio Melo <guto@gutomelo.com>
2023-11-14 17:11:34 -0300

    Upadate App ByteBank - part 1

---
+++ Java/Java - 04 - Java and JDBC working with a database/bytebank/src/main/java/br/com/alura/bytebank/ConexaoDB.java
@@ -0,0 +1,22 @@
+package br.com.alura.bytebank;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDB {
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=root");
+
+            System.out.println("Conexão estabelecida com o banco de dados!");
+
+            connection.close();
+
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+    }
+}
